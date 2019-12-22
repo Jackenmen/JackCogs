0c82125cca4c69df8fa2677affae31dc83e33fbc
jack1142 <6032823+jack1142@users.noreply.github.com>
2019-12-22 23:51:57 +0100

    feat(linkwarner): initial release, commands for all settings added

---
+++ linkwarner/data_classes.py
@@ -0,0 +1,226 @@
+import re
+from string import Template
+from typing import Dict, Iterable, Optional, Pattern, Set
+
+
+class GuildData:
+    """Cached guild data.
+
+    Attributes
+    ----------
+    id: `int`
+        Guild ID.
+    enabled: `bool`
+        Is LinkWarner enabled for this guild.
+    exclude_roles: `set` of `int`
+        Role IDs that should be excluded from filtering in this guild
+    exclude_domains: `set` of `str`
+        Domains that are excluded from filtering in this guild.
+    domains_filter: `Pattern[str]`
+        Compiled regex to check if domain is excluded from filtering.
+    warn_message: `str`
+        Warn message for this guild.
+    warn_message_template: `Template`, optional
+        Warning message template if message is set, otherwise `None`.
+
+    """
+    __slots__ = (
+        "id",
+        "enabled",
+        "exclude_roles",
+        "exclude_domains",
+        "domains_filter",
+        "warn_message",
+        "warn_message_template",
+        "channel_cache",
+    )
+
+    def __init__(
+        self,
+        guild_id: int,
+        *,
+        enabled: bool,
+        exclude_roles: Iterable[int],
+        exclude_domains: Iterable[str],
+        warn_message: str,
+    ) -> None:
+        # TODO: put channel cache in guild data object
+        self.id: int = guild_id
+        self.enabled: bool
+        self.exclude_roles: Set[int]
+        self.exclude_domains: Set[str]
+        self.domains_filter: Optional[Pattern[str]]
+        self.warn_message: str
+        self.warn_message_template: Optional[Template]
+        self.channel_cache: Dict[int, ChannelData] = {}
+
+        self.update_enabled_state(enabled)
+        self.update_excluded_roles(exclude_roles)
+        self.update_excluded_domains(exclude_domains)
+        self.update_warn_message(warn_message)
+
+    def update_enabled_state(self, enabled: bool) -> None:
+        self.enabled = enabled
+
+        # update channel cache with new enabled state
+        for channel_data in self.channel_cache.values():
+            channel_data.update_ignore_state()
+
+    def update_excluded_roles(self, exclude_roles: Iterable[int]) -> None:
+        self.exclude_roles = set(exclude_roles)
+
+    def add_excluded_roles(self, to_add: Iterable[int]) -> None:
+        self.exclude_roles.update(to_add)
+
+    def remove_excluded_roles(self, to_remove: Iterable[int]) -> None:
+        self.exclude_roles.difference_update(to_remove)
+
+    def update_excluded_domains(
+        self, exclude_domains: Optional[Iterable[str]] = None
+    ) -> None:
+        if exclude_domains is not None:
+            self.exclude_domains = set(exclude_domains)
+
+        if self.exclude_domains:
+            self.domains_filter = re.compile(
+                f"^({'|'.join(re.escape(domain) for domain in self.exclude_domains)})",
+                flags=re.I,
+            )
+        else:
+            self.domains_filter = None
+
+        # update channel cache with new excluded domains set
+        for channel_data in self.channel_cache.values():
+            channel_data.update_excluded_domains()
+
+    def add_excluded_domains(self, to_add: Iterable[str]) -> None:
+        self.exclude_domains.update(to_add)
+        self.update_excluded_domains()
+
+    def remove_excluded_domains(self, to_remove: Iterable[str]) -> None:
+        self.exclude_domains.difference_update(to_remove)
+        self.update_excluded_domains()
+
+    def update_warn_message(self, warn_message: str) -> None:
+        self.warn_message = warn_message
+        if warn_message:
+            self.warn_message_template = Template(warn_message)
+        else:
+            self.warn_message_template = None
+
+        # update channel cache with new warn message
+        for channel_data in self.channel_cache.values():
+            channel_data.update_warn_message()
+
+
+class ChannelData:
+    """Cached channel data.
+
+    Attributes
+    ----------
+    id: `int`
+        Channel ID.
+    ignore: `bool`
+        Is channel ignored when checking for links.
+    channel_exclude_domains: `set` of `str`
+        Domains excluded from filtering specifically in this channel.
+    warn_message: `str`
+        Warn message specifically in this channel.
+    guild_data: `GuildData`
+        Guild data for the guild the channel is in.
+    enabled: `bool`
+        Is channel checked for links (includes guild settings).
+    exclude_domains: `set` of `str`
+        Domains that are excluded from filtering
+        in this channel (includes guild settings).
+    domains_filter: `Pattern[str]`
+        Compiled regex to check if domain is excluded
+        from filtering (includes guild settings).
+    warn_message_template: `Template`, optional
+        Warning message template if warn message is set
+        in guild or channel, otherwise `None`.
+
+    """
+    __slots__ = (
+        "id",
+        "ignore",
+        "channel_exclude_domains",
+        "warn_message",
+        "guild_data",
+        "enabled",
+        "exclude_domains",
+        "domains_filter",
+        "warn_message_template",
+    )
+
+    def __init__(
+        self,
+        channel_id: int,
+        *,
+        ignore: bool,
+        exclude_domains: Iterable[str],
+        warn_message: str,
+        guild_data: GuildData,
+    ) -> None:
+        # Channel settings without guild-inherited settings
+        self.id: int = channel_id
+        self.ignore: bool = ignore
+        self.channel_exclude_domains: Set[str] = set(exclude_domains)
+        self.warn_message: str = warn_message
+        # Settings that apply to channel with guild-inherited settings
+        self.guild_data: GuildData
+        self.enabled: bool
+        self.exclude_domains: Set[str]
+        self.domains_filter: Optional[Pattern[str]]
+        self.warn_message_template: Optional[Template]
+
+        self.update_guild_data(guild_data)
+
+    def update_guild_data(self, guild_data: Optional[GuildData] = None) -> None:
+        if guild_data is not None:
+            self.guild_data = guild_data
+        self.update_ignore_state()
+        self.update_excluded_domains()
+        self.update_warn_message()
+
+    def update_ignore_state(self, ignore: Optional[bool] = None) -> None:
+        if ignore is not None:
+            self.ignore = ignore
+
+        self.enabled = self.guild_data.enabled and not self.ignore
+
+    def update_excluded_domains(
+        self, exclude_domains: Optional[Iterable[str]] = None
+    ) -> None:
+        guild_data = self.guild_data
+        if exclude_domains is not None:
+            self.channel_exclude_domains = set(exclude_domains)
+
+        if self.channel_exclude_domains:
+            self.exclude_domains = (
+                guild_data.exclude_domains | self.channel_exclude_domains
+            )
+            self.domains_filter = re.compile(
+                f"^({'|'.join(re.escape(domain) for domain in self.exclude_domains)})",
+                flags=re.I,
+            )
+        else:
+            self.exclude_domains = guild_data.exclude_domains
+            self.domains_filter = guild_data.domains_filter
+
+    def add_excluded_domains(self, to_add: Iterable[str]) -> None:
+        self.channel_exclude_domains.update(to_add)
+        self.update_excluded_domains()
+
+    def remove_excluded_domains(self, to_remove: Iterable[str]) -> None:
+        self.channel_exclude_domains.difference_update(to_remove)
+        self.update_excluded_domains()
+
+    def update_warn_message(self, warn_message: Optional[str] = None) -> None:
+        if warn_message is not None:
+            self.warn_message = warn_message
+
+        if warn_message:
+            self.warn_message_template = Template(self.warn_message)
+        else:
+            self.warn_message_template = self.guild_data.warn_message_template
--- linkwarner/linkwarner.py
+++ linkwarner/linkwarner.py
@@ -1,63 +1,18 @@
 import logging
-import re
 from string import Template
-from typing import List, Optional
+from typing import Dict, cast
 
 import discord
-from redbot.core import commands, modlog
+from redbot.core import checks, commands, modlog
 from redbot.core.bot import Red
 from redbot.core.config import Config
+from redbot.core.utils.chat_formatting import box
 from redbot.core.utils.common_filters import URL_RE
+
+from .data_classes import ChannelData, GuildData
 
 
 log = logging.getLogger("red.jackcogs.linkwarner")
-
-
-class GuildData:
-    def __init__(
-        self,
-        *,
-        enabled: bool,
-        exclude_roles: List[int],
-        exclude_domains: List[str],
-        warn_message: str,
-    ):
-        self.enabled = enabled
-        self.exclude_roles = set(exclude_roles)
-        self.exclude_domains = set(exclude_domains)
-        self.domains_filter = None
-        if self.exclude_domains:
-            self.domains_filter = re.compile(
-                f"^({'|'.join(re.escape(domain) for domain in self.exclude_domains)})",
-                flags=re.I,
-            )
-        self.warn_message = Template(warn_message)
-
-
-class ChannelData:
-    def __init__(
-        self,
-        *,
-        ignore: bool,
-        exclude_domains: List[str],
-        warn_message: str,
-        guild_data: GuildData,
-    ):
-        self.enabled = guild_data.enabled and not ignore
-        self.guild_data = guild_data
-        if exclude_domains:
-            self.exclude_domains = guild_data.exclude_domains | set(exclude_domains)
-            self.domains_filter = re.compile(
-                f"^({'|'.join(re.escape(domain) for domain in self.exclude_domains)})",
-                flags=re.I,
-            )
-        else:
-            self.exclude_domains = guild_data.exclude_domains
-            self.domains_filter = guild_data.domains_filter
-        if warn_message:
-            self.warn_message = Template(warn_message)
-        else:
-            self.warn_message = guild_data.warn_message
 
 
 class LinkWarner(commands.Cog):
@@ -70,10 +25,10 @@ class LinkWarner(commands.Cog):
             enabled=False, exclude_roles=[], exclude_domains=[], warn_message=""
         )
         self.config.register_channel(ignore=False, exclude_domains=[], warn_message="")
-        self.guild_cache = {}
-        self.channel_cache = {}
+        self.guild_cache: Dict[int, GuildData] = {}
 
     async def initialize(self):
+        # TODO: possibly load guild data in cache on load?
         try:
             await modlog.register_casetype(
                 name="linkwarn",
@@ -84,45 +39,348 @@ class LinkWarner(commands.Cog):
         except RuntimeError:
             pass
 
-    async def get_guild_data(
-        self, guild: discord.Guild, force_refresh: bool = False
-    ) -> GuildData:
-        if not force_refresh:
-            try:
-                return self.guild_cache[guild.id]
-            except KeyError:
-                pass
+    async def get_guild_data(self, guild: discord.Guild) -> GuildData:
+        # I wonder what's memory footprint of having all those GuildData objects
+        # for the guilds that don't use this cog at all...
+        try:
+            return self.guild_cache[guild.id]
+        except KeyError:
+            pass
 
         guild_settings = await self.config.guild(guild).all()
-        data = self.guild_cache[guild.id] = GuildData(**guild_settings)
+        data = self.guild_cache[guild.id] = GuildData(guild.id, **guild_settings)
         return data
 
-    async def get_channel_data(
-        self, channel: discord.TextChannel, force_refresh: bool = False
-    ) -> ChannelData:
-        if not force_refresh:
-            try:
-                return self.channel_cache[channel.id]
-            except KeyError:
-                pass
+    async def get_channel_data(self, channel: discord.TextChannel) -> ChannelData:
+        guild_data = await self.get_guild_data(channel.guild)
+        try:
+            return guild_data.channel_cache[channel.id]
+        except KeyError:
+            pass
 
         channel_settings = await self.config.channel(channel).all()
-        guild_data = await self.get_guild_data(
-            channel.guild, force_refresh=force_refresh
+        data = ChannelData(channel.id, guild_data=guild_data, **channel_settings)
+        guild_data.channel_cache[channel.id] = data
+
+        return data
+
+    @commands.group()
+    @checks.admin()
+    async def linkwarner(self, ctx: commands.Context) -> None:
+        """Settings for LinkWarner cog."""
+
+    @linkwarner.command(name="state")
+    async def linkwarner_state(
+        self, ctx: commands.Context, enabled: bool = None
+    ) -> None:
+        """Set if LinkWarner should be enabled for this guild,
+        if used without a setting, this will show the current state."""
+        guild_data = await self.get_guild_data(ctx.guild)
+        if enabled is None:
+            if guild_data.enabled:
+                message = "Bot filters links in this server."
+            else:
+                message = "Bot doesn't filter links in this server."
+            await ctx.send(message)
+            return
+
+        await self.config.guild(ctx.guild).enabled.set(enabled)
+        guild_data.update_enabled_state(enabled)
+
+        if enabled:
+            message = "Bot will now filter links in this server."
+        else:
+            message = "Bot will no longer filter links in this server."
+        await ctx.send(message)
+
+    @linkwarner.command(name="setmessage")
+    async def linkwarner_setmessage(
+        self, ctx: commands.Context, *, message: str
+    ) -> None:
+        """Set link warning message.
+
+        Those fields will get replaced automatically:
+        $mention - Mention the user who sent the message with a link
+        $username - The user's display name
+        $server - The name of the server
+        """
+        await self.config.guild(ctx.guild).warn_message.set(message)
+        guild_data = await self.get_guild_data(ctx.guild)
+        guild_data.update_warn_message(message)
+
+        # we've just set the template, it can't be None
+        tmpl = cast(Template, guild_data.warn_message_template)
+        content = tmpl.safe_substitute(
+            mention=ctx.author.mention, username=str(ctx.author), server=ctx.guild.name
+        )
+        await ctx.send("Link warning message set, sending a test message here...")
+        await ctx.send(content)
+
+    @linkwarner.command(name="unsetmessage")
+    async def linkwarner_unsetmessage(self, ctx: commands.Context) -> None:
+        """Unset link warning message."""
+        await self.config.guild(ctx.guild).warn_message.set("")
+        guild_data = await self.get_guild_data(ctx.guild)
+        guild_data.update_warn_message("")
+
+        await ctx.send("Link warning message unset.")
+
+    # TODO: make this as whitelist/blacklist too
+
+    @linkwarner.group(name="excludedroles")
+    async def linkwarner_excludedroles(self, ctx: commands.Context):
+        """Settings for roles that are excluded from getting filtered."""
+
+    @linkwarner_excludedroles.command(name="list")
+    async def linkwarner_excludedroles_list(self, ctx: commands.Context) -> None:
+        """List roles that are excluded from getting filtered."""
+        guild_data = await self.get_guild_data(ctx.guild)
+
+        excluded_role_ids = guild_data.exclude_roles
+        valid_roles = tuple(r for r in ctx.guild.roles if r.id in excluded_role_ids)
+        valid_role_ids = set(r.id for r in valid_roles)
+        if excluded_role_ids != valid_role_ids:
+            await self.config.guild(ctx.guild).exclude_roles.set(list(valid_role_ids))
+            guild_data.update_excluded_roles(valid_role_ids)
+
+        if not valid_roles:
+            await ctx.send(
+                "There are no roles excluded from link filtering on this server."
+            )
+            return
+        msg = "\n".join(f"+ {r.name}" for r in valid_roles)
+        await ctx.send(
+            box(f"Roles excluded from link filtering on this server:\n{msg}", "diff")
         )
 
-        data = self.channel_cache[channel.id] = ChannelData(
-            **channel_settings, guild_data=guild_data
+    @linkwarner_excludedroles.command(name="add")
+    async def linkwarner_excludedroles_add(
+        self, ctx: commands.Context, *roles: discord.Role
+    ) -> None:
+        """Add roles that will be excluded from getting filtered."""
+        if not roles:
+            await ctx.send_help()
+            return
+
+        guild_data = await self.get_guild_data(ctx.guild)
+        guild_data.add_excluded_roles(role.id for role in roles)
+        await self.config.guild(ctx.guild).exclude_roles.set(
+            list(guild_data.exclude_roles)
         )
-        return data
+
+        await ctx.send("Excluded roles updated.")
+
+    @linkwarner_excludedroles.command(name="remove", aliases=["delete"])
+    async def linkwarner_excludedroles_remove(
+        self, ctx: commands.Context, *roles: discord.Role
+    ) -> None:
+        """Remove roles that will be excluded from getting filtered."""
+        if not roles:
+            await ctx.send_help()
+            return
+
+        guild_data = await self.get_guild_data(ctx.guild)
+        guild_data.remove_excluded_roles(role.id for role in roles)
+        await self.config.guild(ctx.guild).exclude_roles.set(
+            list(guild_data.exclude_roles)
+        )
+
+        await ctx.send("Excluded roles updated.")
+
+    # TODO: change this to support both whitelisting and blacklisting
+
+    @linkwarner.group(name="excludeddomains")
+    async def linkwarner_excludeddomains(self, ctx: commands.Context):
+        """Settings for domains that are excluded from getting filtered."""
+
+    @linkwarner_excludeddomains.command(name="list")
+    async def linkwarner_excludeddomains_list(self, ctx: commands.Context) -> None:
+        """List domains that are excluded from getting filtered."""
+        guild_data = await self.get_guild_data(ctx.guild)
+
+        excluded_domains = guild_data.exclude_domains
+        if not excluded_domains:
+            await ctx.send(
+                "There are no domains excluded from link filtering on this server."
+            )
+            return
+        msg = "\n".join(f"+ {domain}" for domain in excluded_domains)
+        await ctx.send(
+            box(f"Domains excluded from link filtering on this server:\n{msg}", "diff")
+        )
+
+    @linkwarner_excludeddomains.command(name="add")
+    async def linkwarner_excludeddomains_add(
+        self, ctx: commands.Context, *domains: str
+    ) -> None:
+        """Add domains that will be excluded from getting filtered."""
+        if not domains:
+            await ctx.send_help()
+            return
+
+        guild_data = await self.get_guild_data(ctx.guild)
+        guild_data.add_excluded_domains(domains)
+        await self.config.guild(ctx.guild).exclude_domains.set(
+            list(guild_data.exclude_domains)
+        )
+
+        await ctx.send("Excluded domains updated.")
+
+    @linkwarner_excludeddomains.command(name="remove", aliases=["delete"])
+    async def linkwarner_excludeddomains_remove(
+        self, ctx: commands.Context, *domains: str
+    ) -> None:
+        """Remove domains that will be excluded from getting filtered."""
+        if not domains:
+            await ctx.send_help()
+            return
+
+        guild_data = await self.get_guild_data(ctx.guild)
+        guild_data.remove_excluded_domains(domains)
+        await self.config.guild(ctx.guild).exclude_domains.set(
+            list(guild_data.exclude_domains)
+        )
+
+        await ctx.send("Excluded domains updated.")
+
+    # TODO: REMOVE THIS BLOODY HERESY (as in, a lot of unnecessary code repetition)
+
+    @linkwarner.group(name="channel")
+    async def linkwarner_channel(self, ctx: commands.Context) -> None:
+        """Channel-specific settings for LinkWarner."""
+
+    @linkwarner_channel.command(name="ignore")
+    async def linkwarner_channel_ignore(
+        self, ctx: commands.Context, channel: discord.TextChannel, ignore: bool = None
+    ) -> None:
+        """Set if LinkWarner should ignore links in provided channel,
+        if used without a setting, this will show the current state."""
+        channel_data = await self.get_channel_data(channel)
+        if ignore is None:
+            if channel_data.ignore:
+                message = f"Bot ignores links in {channel.mention} channel."
+            else:
+                message = f"Bot filters links in {channel.mention} channel."
+            await ctx.send(message)
+            return
+
+        await self.config.channel(channel).ignore.set(ignore)
+        channel_data.update_ignore_state(ignore)
+
+        if ignore:
+            message = f"Bot will now ignore links in {channel.mention} channel."
+        else:
+            message = f"Bot will now filter links in {channel.mention} channel."
+        await ctx.send(message)
+
+    @linkwarner_channel.command(name="setmessage")
+    async def linkwarner_channel_setmessage(
+        self, ctx: commands.Context, channel: discord.TextChannel, *, message: str
+    ) -> None:
+        """Set link warning message for provided channel.
+
+        Those fields will get replaced automatically:
+        $mention - Mention the user who sent the message with a link
+        $username - The user's display name
+        $server - The name of the server
+        """
+        await self.config.channel(channel).warn_message.set(message)
+        channel_data = await self.get_channel_data(channel)
+        channel_data.update_warn_message(message)
+
+        # we've just set the template, it can't be None
+        tmpl = cast(Template, channel_data.warn_message_template)
+        content = tmpl.safe_substitute(
+            mention=ctx.author.mention, username=str(ctx.author), server=ctx.guild.name
+        )
+        await ctx.send("Link warning message set, sending a test message here...")
+        await ctx.send(content)
+
+    @linkwarner_channel.command(name="unsetmessage")
+    async def linkwarner_channel_unsetmessage(
+        self, ctx: commands.Context, channel: discord.TextChannel
+    ) -> None:
+        """Unset link warning message for provided channel."""
+        await self.config.channel(channel).warn_message.set("")
+        channel_data = await self.get_channel_data(channel)
+        channel_data.update_warn_message("")
+
+        await ctx.send("Link warning message unset.")
+
+    @linkwarner_channel.group(name="excludeddomains")
+    async def linkwarner_channel_excludeddomains(self, ctx: commands.Context):
+        """Settings for domains that are excluded from getting filtered."""
+
+    @linkwarner_channel_excludeddomains.command(name="list")
+    async def linkwarner_channel_excludeddomains_list(
+        self, ctx: commands.Context, channel: discord.TextChannel
+    ) -> None:
+        """List domains that are excluded from getting filtered in provided channel."""
+        channel_data = await self.get_channel_data(channel)
+
+        excluded_domains = channel_data.channel_exclude_domains
+        if not excluded_domains:
+            await ctx.send(
+                "There are no domains excluded from link filtering on this channel"
+                " (doesn't include domains excluded from filtering in whole guild)."
+            )
+            return
+        msg = "\n".join(f"+ {domain}" for domain in excluded_domains)
+        await ctx.send(
+            box(
+                f"Domains excluded from link filtering on #{channel.name} channel"
+                " (doesn't include domains excluded from filtering in whole guild):\n"
+                f"{msg}",
+                "diff",
+            )
+        )
+
+    @linkwarner_channel_excludeddomains.command(name="add")
+    async def linkwarner_channel_excludeddomains_add(
+        self, ctx: commands.Context, channel: discord.TextChannel, *domains: str
+    ) -> None:
+        """Add domains that will be excluded
+        from getting filtered in provided channel.
+        """
+        if not domains:
+            await ctx.send_help()
+            return
+
+        channel_data = await self.get_channel_data(channel)
+        channel_data.add_excluded_domains(domains)
+        await self.config.channel(channel).exclude_domains.set(
+            list(channel_data.channel_exclude_domains)
+        )
+
+        await ctx.send("Excluded domains updated.")
+
+    @linkwarner_channel_excludeddomains.command(name="remove", aliases=["delete"])
+    async def linkwarner_channel_excludeddomains_remove(
+        self, ctx: commands.Context, channel: discord.TextChannel, *domains: str
+    ) -> None:
+        """Remove domains that will be excluded
+        from getting filtered in provided channel.
+        """
+        if not domains:
+            await ctx.send_help()
+            return
+
+        channel_data = await self.get_channel_data(channel)
+        channel_data.remove_excluded_domains(domains)
+        await self.config.channel(channel).exclude_domains.set(
+            list(channel_data.channel_exclude_domains)
+        )
+
+        await ctx.send("Excluded domains updated.")
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
-        author = message.author
-        channel = message.channel
         guild = message.guild
-        if guild is None or author.bot:
+        if guild is None or message.author.bot:
             return
+        # casting with the knowledge we got above
+        author = cast(discord.Member, message.author)
+        channel = cast(discord.TextChannel, message.channel)
         if await self.bot.is_automod_immune(message):
             return
 
@@ -132,7 +390,7 @@ class LinkWarner(commands.Cog):
             return
 
         common_roles = guild_data.exclude_roles.intersection(
-            role.id for role in message.author.roles
+            role.id for role in author.roles
         )
         if common_roles:
             return
@@ -151,12 +409,16 @@ class LinkWarner(commands.Cog):
                     channel.id,
                     guild.id,
                 )
-            warn_message = channel_data.warn_message
-            if not warn_message:
+            template = channel_data.warn_message_template
+            if template is None:
                 return
 
             try:
-                await channel.send(warn_message.safe_substitute(mention=author.mention))
+                await channel.send(
+                    template.safe_substitute(
+                        mention=author.mention, username=str(author), server=guild.name
+                    )
+                )
             except discord.Forbidden:
                 log.error(
                     "Bot can't send messages in channel with ID %s (guild ID: %s)",
