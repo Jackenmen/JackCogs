740635a03c45df370943b30359692abac74eaab2
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-01-02 01:59:47 +0100

    enhance(cogboard): raise `CogLoadError` on missing deps

--- cogboard/__init__.py
+++ cogboard/__init__.py
@@ -1,6 +1,14 @@
 from redbot.core.bot import Red
 
-from .cogboard import CogBoard
+try:
+    from .cogboard import CogBoard
+except ModuleNotFoundError as e:
+    if e.name == "typing_extensions":
+        raise CogLoadError(
+            "You need `typing_extensions` pip package to run this cog."
+            " Downloader *should* have handled this for you."
+        )
+    raise
 
 
 def setup(bot: Red) -> None:
