d6f0b60116d1b83441e9911bcdd0d5ec9669886c
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-07-02 21:55:52 +0200

    enhance(mee6rank): update `Point` class with the one from `rlstats`

--- mee6rank/figures.py
+++ mee6rank/figures.py
@@ -1,42 +1,29 @@
-__all__ = "Point"
+from __future__ import annotations
+
+from typing import Any, NamedTuple, Tuple
+
+__all__ = ("Point",)
 
 
-class Point:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+class Point(NamedTuple):
+    x: int
+    y: int
 
-    def __getitem__(self, item) -> int:
-        return (self.x, self.y)[item]
-
-    def __iter__(self):
-        for axis in (self.x, self.y):
-            yield axis
-
-    def __repr__(self) -> str:
-        return f"Point({self.x}, {self.y})"
-
-    def __len__(self) -> int:
-        return 2
-
-    def __add__(self, other) -> "Point":
+    def __add__(self, other: Any) -> Point:
         return self.__class__(self.x + other[0], self.y + other[1])
 
-    def __radd__(self, other) -> "Point":
-        return self + other
+    def __radd__(self, other: Any) -> Point:
+        return self.__add__(other)
 
-    def __sub__(self, other) -> "Point":
+    def __sub__(self, other: Any) -> Point:
         return self.__class__(self.x - other[0], self.y - other[1])
 
-    def __rsub__(self, other) -> "Point":
+    def __rsub__(self, other: Any) -> Point:
         return self.__class__(other[0] - self.x, other[1] - self.y)
 
-    def __neg__(self) -> "Point":
+    def __neg__(self) -> Point:
         return self.__class__(-self.x, -self.y)
 
-    def to_tuple(self):
+    def to_tuple(self) -> Tuple[int, int]:
+        # while Point is a tuple, it defines incompatible add and sub operations
         return (self.x, self.y)
-
-    @classmethod
-    def from_tuple(cls, tup: tuple):
-        return cls(*tup[:2])
