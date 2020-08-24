17aadd870419d7cdc6587ef5594be115599fd31b
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-08-24 18:20:08 +0200

    build: stop vendoring discord.py stubs

--- .stubs/discord/object.pyi
+++
@@ -1,9 +0,0 @@
-from .mixins import Hashable
-import datetime
-
-class Object(Hashable):
-    id: int
-
-    def __init__(self, id: int) -> None: ...
-    @property
-    def created_at(self) -> datetime.datetime: ...
--- .stubs/discord/relationship.pyi
+++
@@ -1,9 +0,0 @@
-from .enums import RelationshipType
-from .user import User
-
-class Relationship:
-    user: User
-    type: RelationshipType
-
-    async def delete(self) -> None: ...
-    async def accept(self) -> None: ...
