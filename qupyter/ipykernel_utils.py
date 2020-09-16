e0937b9ff29b9587cef82950743d81f16b349515
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-09-16 17:54:24 +0200

    feat(qupyter): add ipykernel utils needed for implementation for Red

---
+++ qupyter/ipykernel_utils.py
@@ -0,0 +1,68 @@
+# Copyright 2018-2020 Jakub Kuczys (https://github.com/jack1142)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from typing import Any, Dict
+
+from ipykernel.kernelapp import IPKernelApp
+from tornado import ioloop
+
+
+def embed_kernel(local_ns: Dict[str, Any], **kwargs: Any) -> RedIPKernelApp:
+    """
+    Embed and start an IPython kernel in a given scope.
+
+    Parameters
+    ----------
+    local_ns: Dict[str, Any]
+        The namespace to load into IPython user namespace.
+    kwargs: various, optional
+        Further keyword args are relayed to the RedIPKernelApp constructor,
+        allowing configuration of the Kernel. Will only have an effect
+        on the first embed_kernel call for a given process.
+    """
+    # get the app if it exists, or set it up if it doesn't
+    if RedIPKernelApp.initialized():
+        app = RedIPKernelApp.instance()
+    else:
+        app = RedIPKernelApp.instance(**kwargs)
+        app.initialize([])
+
+    app.kernel.user_ns = local_ns
+    app.shell.set_completer_frame()
+    app.start()
+    return app
+
+
+class RedIPKernelApp(IPKernelApp):
+    kernel_name = "IPython Kernel for Red"
+
+    def start(self) -> None:
+        """
+        Same as IPKernelApp.start(),
+        but doesn't try to run the event loop on its own.
+        """
+        if self.subapp is not None:
+            raise RuntimeError("RedIPKernelApp does not support subapps!")
+        if self.trio_loop:
+            raise RuntimeError("RedIPKernelApp does not support trio loop!")
+
+        if self.poller is not None:
+            self.poller.start()
+        self.kernel.start()
+        self.io_loop = ioloop.IOLoop.current()
+
+    def init_sys_modules(self) -> None:
+        """Explicitly overwrite this to do nothing in embedded app."""
