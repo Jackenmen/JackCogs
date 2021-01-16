4087319b10c507ab70bf432c37753103614f9549
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-01-16 02:45:16 +0100

    fix(linkwarner): add `__red_end_user_data_statement__`

--- linkwarner/__init__.py
+++ linkwarner/__init__.py
@@ -12,9 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
+from pathlib import Path
+
 from redbot.core.bot import Red
 
 from .linkwarner import LinkWarner
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
 async def setup(bot: Red):
