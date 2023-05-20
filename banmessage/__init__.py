44fb11178794a2507b46e5ae794db11b672867a7
Jakub Kuczys <me@jacken.men>
2023-05-20 15:28:40 +0200

    refactor: use get_end_user_data_statement_or_raise()

--- banmessage/__init__.py
+++ banmessage/__init__.py
@@ -12,15 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-from pathlib import Path
-
 from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement_or_raise
 
 from .banmessage import BanMessage
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+__red_end_user_data_statement__ = get_end_user_data_statement_or_raise(__file__)
 
 
 async def setup(bot: Red) -> None:
