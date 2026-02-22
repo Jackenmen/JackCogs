cdd964ec565536300d6b323a68646c1b3ec2b92c
Jakub Kuczys <me@jacken.men>
2026-02-22 02:56:15 +0100

    add missing await

--- fluxerbridge/fluxerbridge.py
+++ fluxerbridge/fluxerbridge.py
@@ -52,7 +52,7 @@ class WebhookAdapter(AsyncWebhookAdapter):
 
     async def request(self, route, *args: Any, **kwargs: Any) -> Any:
         route.url = self.__base + route.url[len(route.BASE) :]
-        return super().request(route, *args, **kwargs)
+        return await super().request(route, *args, **kwargs)
 
 
 class Webhook(discord.Webhook):
