bc498d9380d1b0f71aa5c31641fb20de268aaa87
Jakub Kuczys <me@jacken.men>
2026-02-22 01:56:03 +0100

    Fix missing initialization

--- fluxerbridge/__init__.py
+++ fluxerbridge/__init__.py
@@ -23,3 +23,4 @@ __red_end_user_data_statement__ = get_end_user_data_statement_or_raise(__file__)
 async def setup(bot: Red) -> None:
     cog = FluxerBridge(bot)
     await bot.add_cog(cog)
+    await cog.initialize()
